24a88aea01f88e0c1487f9810ba6249cd6639816
xuRookie <xu128523@163.com>
2020-08-11 20:52:57 +0800

    本地mongodb

--- model/connect.js
+++ model/connect.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 // 配置登录权限
 // var config = require('config')
 
-var mongodbUrl = 'mongodb://www.heyitong904.com:27017/blog'
+var mongodbUrl = 'mongodb://localhost:27017/blog'
 // var user = config.get('db.user')
 // var pwd = config.get('db.pwd')
 // var host = config.get('db.host')
